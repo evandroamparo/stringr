e89270de426810a2a097c0f3910c9039c679245a
Evandro da Silva Amparo <evandro.amparo@gmail.com>
2012-03-24 13:47:41 -0300

    Reorganiza??o do c?digo fonte

--- Test/StringrTests.dpr
+++ Test/StringrTests.dpr
@@ -20,16 +20,21 @@ uses
   GUITestRunner,
   TextTestRunner,
   TestStringr in 'TestStringr.pas',
-  UStringr in '..\UStringr.pas',
   TestAtributoLength in 'TestAtributoLength.pas',
   TestAtributoCase in 'TestAtributoCase.pas',
   TestAtributoFormat in 'TestAtributoFormat.pas',
   TestParser in 'TestParser.pas',
-  UDefaultParser in '..\UDefaultParser.pas',
-  UAtributoLength in '..\UAtributoLength.pas',
-  UAtributoCase in '..\UAtributoCase.pas',
-  UAtributoFormat in '..\UAtributoFormat.pas',
-  UParametro in '..\UParametro.pas';
+  UAtributoCase in '..\src\UAtributoCase.pas',
+  UAtributoFormat in '..\src\UAtributoFormat.pas',
+  UAtributoLength in '..\src\UAtributoLength.pas',
+  UDefaultParser in '..\src\UDefaultParser.pas',
+  UParametro in '..\src\UParametro.pas',
+  UStringr in '..\src\UStringr.pas',
+  RegExpr in '..\src\Utils\RegExpr.pas',
+  UCustomParser in '..\src\UCustomParser.pas',
+  UElemento in '..\src\UElemento.pas',
+  UAtributo in '..\src\UAtributo.pas',
+  UTexto in '..\src\UTexto.pas';
 
 {$R *.RES}
 
